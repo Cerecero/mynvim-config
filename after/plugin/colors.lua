164f0a9cbb3eac4067b8e8c4c248adf5defe7237
Cerecero <ale.rey.cer@outlook.com>
2024-08-27 17:04:11 -0600

    added a new theme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,11 +1,16 @@
 -- Color Theme --
-function ColorTheme(color)
-		color = color or "nord"
-		vim.cmd.colorscheme(color)
+--function ColorTheme(color)
+--		color = color or "onedark" --"nord"
+--		vim.cmd.colorscheme(color)
+--
+--      vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+--      vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+--end
 
-  --      vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-    --    vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
-end
+--ColorTheme()
 
-ColorTheme()
 
+require('onedark').setup {
+    style = 'cool',
+}
+require('onedark').load()
