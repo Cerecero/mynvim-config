5c6615128a683ac43b45288dc23906e224d54cfd
Cerecero <ale.rey.cer@outlook.com>
2025-02-01 16:21:05 -0600

    added highlight to lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -15,18 +15,18 @@ cmp.setup({
     },
     snippet = {
         expand = function(args)
-            require('luasnip').lsp_expand(args.body)         -- For `luasnip` users.
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
         end,
     },
     mapping = cmp.mapping.preset.insert({
         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.complete(),         -- Invoke Completion Manually
+        ["<C-Space>"] = cmp.mapping.complete(), -- Invoke Completion Manually
     }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
-        { name = 'luasnip' },         -- For luasnip users.
+        { name = 'luasnip' }, -- For luasnip users.
     }, {
         { name = 'buffer' },
     })
@@ -37,23 +37,28 @@ cmp.setup({
 local lsp_attach = function(client, bufnr)
     local opts = { buffer = bufnr }
 
-    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts, {desc = 'Display hover information'})
-    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts, {desc = '[G]oto [D]efinition'})
-    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts, {desc ='[G]oto [D]eclaration'})
-    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts, {desc = 'List all the implementations'})
-    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts, {desc = 'Jumps to definition'})
-    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts, {desc = 'list all the references'})
-    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts, {desc= 'Displays signature help'})
-    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts, {desc= 'Renames all references for this symbol'})
-    vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts, {desc= 'Formats code'})
-    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts, {desc= 'Selects a code action available'})
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts, { desc = 'Display hover information' })
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts, { desc = '[G]oto [D]efinition' })
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts, { desc = '[G]oto [D]eclaration' })
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts, { desc = 'List all the implementations' })
+    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts, { desc = 'Jumps to definition' })
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts, { desc = 'list all the references' })
+    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts, { desc = 'Displays signature help' })
+    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts,
+        { desc = 'Renames all references for this symbol' })
+    vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts, { desc = 'Formats code' })
+    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts, { desc =
+    'Selects a code action available' })
 
-    vim.keymap.set('n', '<leader>dn', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts, {desc='Jumps to the next diagnostic'})
-    vim.keymap.set('n', '<leader>dp', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts, {desc='Jumps to the previous diagnostic'})
-    vim.keymap.set('n', '<leader>df', '<cmd>lua vim.diagnostic.open_float()<CR>', opts, {desc='Opens a floating windows for this diagnostic'})
+    vim.keymap.set('n', '<leader>dn', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts,
+        { desc = 'Jumps to the next diagnostic' })
+    vim.keymap.set('n', '<leader>dp', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts,
+        { desc = 'Jumps to the previous diagnostic' })
+    vim.keymap.set('n', '<leader>df', '<cmd>lua vim.diagnostic.open_float()<CR>', opts,
+        { desc = 'Opens a floating windows for this diagnostic' })
 
-    vim.keymap.set('n', '<leader>dd', '<cmd>lua vim.diagnostic.enable(false)<CR>',opts, {desc='Disable diagnostics'})
-    vim.keymap.set('n', '<leader>de', '<cmd>lua vim.diagnostic.enable(true)<CR>',opts, {desc='Enable diagnostics'})
+    vim.keymap.set('n', '<leader>dd', '<cmd>lua vim.diagnostic.enable(false)<CR>', opts, { desc = 'Disable diagnostics' })
+    vim.keymap.set('n', '<leader>de', '<cmd>lua vim.diagnostic.enable(true)<CR>', opts, { desc = 'Enable diagnostics' })
 end
 
 lsp_zero.extend_lspconfig({
@@ -76,4 +81,55 @@ cmp.setup({
         end,
     },
     mapping = cmp.mapping.preset.insert({}),
-}) 
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+    callback = function(event)
+        local map = function(keys, func, desc, mode)
+            mode = mode or 'n'
+            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+        end
+        vim.api.nvim_set_hl(0, 'LspReferenceText', { bg = '#403c64', underline = false })
+        vim.api.nvim_set_hl(0, 'LspReferenceRead', { bg = '#403c64', underline = false })
+        vim.api.nvim_set_hl(0, 'LspReferenceWrite', { bg = '#403c64', underline = false })
+        -- The following two autocommands are used to highlight references of the
+        -- word under your cursor when your cursor rests there for a little while.
+        --    See `:help CursorHold` for information about when this is executed
+        -- ADD THIS TO MY OWN MAYBE
+        -- When you move your cursor, the highlights will be cleared (the second autocommand).
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+            local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
+            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+                buffer = event.buf,
+                group = highlight_augroup,
+                callback = vim.lsp.buf.document_highlight,
+            })
+
+            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+                buffer = event.buf,
+                group = highlight_augroup,
+                callback = vim.lsp.buf.clear_references,
+            })
+
+            vim.api.nvim_create_autocmd('LspDetach', {
+                group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+                callback = function(event2)
+                    vim.lsp.buf.clear_references()
+                    vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+                end,
+            })
+        end
+
+        -- The following code creates a keymap to toggle inlay hints in your
+        -- code, if the language server you are using supports them
+        --
+        -- This may be unwanted, since they displace some of your code
+        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+            map('<leader>th', function()
+                vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+            end, '[T]oggle Inlay [H]ints')
+        end
+    end,
+})
