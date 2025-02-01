b936c8e98b8939a008ca53feb99dcd9987028e8e
Cerecero <ale.rey.cer@outlook.com>
2025-02-01 16:25:41 -0600

    added fuzzy finder on the buffer

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,5 @@
 -- Telescope --
-require('telescope').setup{
+require('telescope').setup {
     -- pickers = {
     --     find_files = {
     --         theme = "dropdown"
@@ -16,24 +16,30 @@ require('telescope').setup{
 }
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.git_files, {desc= 'Git files'})
+vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Git files' })
 vim.keymap.set('n', '<leader>ps', function()
-		builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end, {desc = 'Project Search'})
-vim.keymap.set('n', '<leader>ds', builtin.lsp_document_symbols, {desc = 'Document Symbols'})
-vim.keymap.set('n', '<leader>ws', builtin.lsp_dynamic_workspace_symbols, {desc = 'Workspace Symbols'})
-vim.keymap.set('n', '<leader>gr', builtin.lsp_references, {desc = 'Goto References'})
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = 'Find Files'})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Find Help Tags'})
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end, { desc = 'Project Search' })
+vim.keymap.set('n', '<leader>ds', builtin.lsp_document_symbols, { desc = 'Document Symbols' })
+vim.keymap.set('n', '<leader>ws', builtin.lsp_dynamic_workspace_symbols, { desc = 'Workspace Symbols' })
+vim.keymap.set('n', '<leader>gr', builtin.lsp_references, { desc = 'Goto References' })
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find Files' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Find Help Tags' })
 
 
-vim.keymap.set('n', '<leader>cf', function ()
+vim.keymap.set('n', '<leader>cf', function()
     local opts = require('telescope.themes').get_ivy({
-            cwd = vim.fn.stdpath("config")
+        cwd = vim.fn.stdpath("config")
     })
     builtin.find_files(opts)
-end, { desc = 'Nvim Config Files'})
+end, { desc = 'Nvim Config Files' })
 
+vim.keymap.set('n', '<leader>/', function()
+    builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        winblend = 10,
+        previewer = false,
+    })
+end, { desc = '[/] Fuzzily search in current buffer' })
 
 -- vim.keymap.set('n', '<leader>fs', builtin.git_status(), {desc = 'Git Status'})
 --
